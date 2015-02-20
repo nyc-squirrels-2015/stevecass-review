0326862522479f80ae8f47db4e2ff7516b5ad5ea
rosapergams <rosa@pergams.com>
2015-02-20 11:54:27 -0500

    Start 'put' AJAX method

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('#create_comment').submit(function(e){
+    e.preventDefault();
+    $form = $(e.target);
+    $.ajax({
+      type: "POST",
+      url: $form.attr('action'),
+      data: $form.serialize()
+    }).done(function(response){
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+      // console.log("response", response)
+    })
+  })
 });
