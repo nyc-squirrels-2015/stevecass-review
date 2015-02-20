fb4063ed8e30623bd0d7f9cdee68f0788cd6d3c7
rosapergams <rosa@pergams.com>
2015-02-20 10:35:00 -0500

    Debug Migrations

--- db/migrate/20150220102823_create_comments.rb
+++ db/migrate/20150220102823_create_comments.rb
@@ -1,6 +1,6 @@
 class CreateComments < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
+    create_table :comments do |t|
       t.string :username
       t.text :content
       t.integer :post_id
